e8b52d84e650b2bd777057192c6af0cd70932bf3
SagarSardarcoder <102857635+SagarSardarcoder@users.noreply.github.com>
2022-10-21 11:53:13 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb+srv://Sagar-functionup:radhaswami123@cluster0.7xlsi.mongodb.net/inotebooks?retryWrites=true&w=majority"
+const mongoURI = ""
 function connectToMongo(){
     mongoose.connect(mongoURI,()=>{
         console.log("connected to mongoDB successfully")
